ad36052727728a15af9d57799d5f612e27816a3f
jaythaceo <jaythaceo@gmail.com>
2016-05-25 21:56:05 -0400

    Initial Commit

---
+++ Politic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Politic
+//
+//  Created by Jason Brooks on 5/25/16.
+//  Copyright © 2016 Jason Brooks. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
